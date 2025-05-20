99b7b6d6f47d2450c407c33f5bcfae9c602d7d7a
Shriyans Ayalasomayajula <shriyans@Shriyanss-MacBook-Pro.local>
2025-05-19 19:05:24 -0700

    Add dependencies

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from chatbot_agent import route_command
 
-def main():
+def main(): # main function that users will run
   
   print("Welcome to the AI Tool Agent!")
-  print("Available commands: calculate <expr>, weather <location>, read file <filename>")
-  print("Type 'exit' to quit.")
+  print("Available commands: calculate (expression), weather (location), read file (filename), or you can ask to search Opsera documentation for information.")
+  print("Type 'exit' or 'quit' to terminate session.")
 
-  while True:
+  while True: # keep asking for input unless they specifically exit or quit. 
     user_input = input(">> ")
     if user_input.strip().lower() in ["exit", "quit"]:
       print("Session terminated")
       break
-    response = route_command(user_input)
+    response = route_command(user_input) # use the route command function that will run the function from chatbot_agent.py
     print(response)
 
-if __name__ == "__main__":
+if __name__ == "__main__": # allows function to run after importing from another file. 
   main()
     
 
